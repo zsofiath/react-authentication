5a07919c908f035073cd9465a0006aaaa037cc62
zsofi <zsofiath@gmail.com>
2022-02-20 11:34:03 +0100

    :sparkles: adding route guard, guarding

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
+import {useContext} from 'react'
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const authCtx = useContext(AuthContext)
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!authCtx.isLogedIn && (<Route path='/auth'>
           <AuthPage />
-        </Route>
+        </Route>)}
         <Route path='/profile'>
-          <UserProfile />
+        {authCtx.isLogedIn &&<UserProfile />}
+        {!authCtx.isLogedIn && <Redirect to='/auth'></Redirect>}
+        </Route>
+        <Route path="*">
+          <Redirect to='/'></Redirect>
         </Route>
       </Switch>
     </Layout>
